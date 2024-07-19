fd9de2e8526badcf40694300b30e4519b9e97a51
Yoga2015 <yoga3333@126.com>
2024-07-19 23:51:29 +0800

    泛型、泛型约束

---
+++ 33-泛型（泛型函数）.ts
@@ -0,0 +1,26 @@
+// 使用 泛型 来 创建一个函数
+
+function id<Type>(value: Type): Type {
+  return value
+}
+
+// 调用 泛型函数
+
+// 1、以 number 类型 调用 泛型函数
+const num = id<number>(10)
+
+// 2、以 string 类型 调用 泛型函数
+const str = id<string>('kk')
+
+// const ret = id<boolean>(false)
+
+
+
+// 简化 调用 泛型函数  
+
+const num1 = id(10)
+const str1 = id('cc')
+
+const str2 = id<number>(26)
+
+// 注意： 当编 译器 无法推断类型 或 推断的类型不准确 时，就需要 显式地传入 类型参数。
---
+++ 34-泛型约束.ts
@@ -0,0 +1,41 @@
+// function People<Type>(value: Type): Type {
+
+//   console.log(value.length);
+
+// }
+
+// 添加 泛型约束 收缩类型，主要有以下两种方式： 1、指定更加具体的类型 2、添加约束
+
+// 1、指定更加具体的类型      (这里的 需求是： 接收什么数据 就返回 什么数据本身 )
+function People<Type>(value: Type[]): Type[] {
+
+  console.log(value.length);    // 2
+
+  return value
+
+}
+
+const p9 = People<number>([200, 300])
+
+
+// 比如： 将 类型 修改为 Type[]（Type类型的数组），因为只要是 数组 就 一定存在 length 属性，因此就可以访问了。
+
+
+// 2、添加约束
+interface ILength {
+  length: number
+}
+
+function Id<Type extends ILength>(value: Type): Type {
+  value.length
+  return value
+}
+
+Id(['a', 'k'])
+Id('abc')
+Id({ name: 'walking', length: 23 })
+
+//  extends 在此的含义不是 继承， 而是变成了 类型变量Type 必须要满足 extends 后面 这个ILength类型的一个约束: 必须具有 length 属性 .
+
+// 错误演示
+// Id(123)    // Argument of type 'number' is not assignable to parameter of type 'ILength'.
