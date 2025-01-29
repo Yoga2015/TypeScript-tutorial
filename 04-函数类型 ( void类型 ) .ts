ea2713c595e146a79d7903f5e1c61d63302c6821
Yoga2015 <yoga3333@126.com>
2025-01-29 23:46:13 +0800

    分析 void类型

--- 04-函数类型 ( void类型 ) .ts
+++ 04-函数类型 ( void类型 ) .ts
@@ -1,11 +1,13 @@
-// 如果 一个函数 ，它没有返回值，那么 这个函数 的 返回值类型 为 void 类型
-// void 单词的意思是 空 的意思，就是 没有返回值
+
 
 function greet(name: string): void {
   console.log('hey', name);
 }
 
-// 什么使用 void 类型 ？
+// 什么是 void 类型 ？怎么使用void类型？
+
+//  void 单词的意思是 空 的意思，当 一个函数 ，它没有返回值，那么 这个函数 的 返回值类型 为 void 类型
 
 // 当 一个函数 没有返回值 的 时候，就应该 使用 void 作为 函数返回值的类型
+
 // void 类型 是 Typescript 里 新增的类型
